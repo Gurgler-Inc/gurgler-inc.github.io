c01e7169b4ec02782ed9da29ffc5795b854678e8
Macangus69 <148728673+Macangus69@users.noreply.github.com>
2023-12-18 22:32:45 -0600

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-workbox.precaching.precacheAndRoute([{ url: '/', revision: 'v1' }]);
+workbox.precaching.precacheAndRoute([{ url: '/', revision: 'v3' }]);
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
